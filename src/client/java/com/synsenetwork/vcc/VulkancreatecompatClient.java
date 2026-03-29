50504d046fbcb6c58f2a31c51590e18ac9cec93f
Kristián Kekeš <gamerix2006@gmail.com>
2026-03-29 21:15:40 +0200

    Switch to Modrinth Maven dependencies, add LWJGL runtime libs, and update Gradle memory settings

--- src/client/java/com/synsenetwork/vcc/VulkancreatecompatClient.java
+++ src/client/java/com/synsenetwork/vcc/VulkancreatecompatClient.java
@@ -1,10 +1,12 @@
 package com.synsenetwork.vcc;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 
 public class VulkancreatecompatClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		Vulkancreatecompat.LOGGER.info("Initializing Create Fly compatibility patches for VulkanMod");
+		ClientLifecycleEvents.CLIENT_STARTED.register(client ->
+			Vulkancreatecompat.LOGGER.info("Initializing Create Fly compatibility patches for VulkanMod"));
 	}
 }
